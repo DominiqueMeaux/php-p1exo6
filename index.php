0d62d5f7551eba87aa88ebbcb622751d7ccf1345
dominiquem <dominique.meaux@gmail.com>
2018-07-26 09:26:27 +0200

    php p1exo6

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8"/>
+    <title>p1exo6</title>
+  </head>
+  <body>
+    <p>
+      bonjour
+      <?php
+        $name = 'dominique';
+        echo $name;
+      ?>
+      , comment vas tu?
+    </p>
+  </body>
+</html>
